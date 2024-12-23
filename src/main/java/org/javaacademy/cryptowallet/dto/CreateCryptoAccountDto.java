d4576e004352fe013a8a3a2fd237c904d209f982
AaronDeluna <aaron_deluna@mail.ru>
2024-12-23 23:00:09 +0300

    Удалено: лишние импорты

--- src/main/java/org/javaacademy/cryptowallet/dto/CreateCryptoAccountDto.java
+++ src/main/java/org/javaacademy/cryptowallet/dto/CreateCryptoAccountDto.java
@@ -2,7 +2,6 @@ package org.javaacademy.cryptowallet.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
 import org.javaacademy.cryptowallet.entity.CryptoCurrency;
 
