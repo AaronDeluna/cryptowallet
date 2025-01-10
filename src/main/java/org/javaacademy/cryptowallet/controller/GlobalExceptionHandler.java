7a41fc7eb0c904705329a9cbe2c54ce58cac9d53
AaronDeluna <aaron_deluna@mail.ru>
2025-01-10 11:38:51 +0300

    Рефакторинг: Удалил лишние импорты из классов

--- src/main/java/org/javaacademy/cryptowallet/controller/GlobalExceptionHandler.java
+++ src/main/java/org/javaacademy/cryptowallet/controller/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package org.javaacademy.cryptowallet.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.javaacademy.cryptowallet.dto.ErrorResponse;
 import org.javaacademy.cryptowallet.exception.CryptoAccountIdExistException;
